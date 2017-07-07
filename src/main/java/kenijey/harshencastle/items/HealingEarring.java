b42e44e38a95e89ff08cdbcd3610c3ea3cd7ae9d
Keny <mylankca1995@gmail.com>
2017-07-07 17:08:53 +0300

    added tooltip for earring and updated lang

--- src/main/java/kenijey/harshencastle/items/HealingEarring.java
+++ src/main/java/kenijey/harshencastle/items/HealingEarring.java
@@ -1,6 +1,12 @@
 package kenijey.harshencastle.items;
 
+import java.util.List;
+
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.TextComponentTranslation;
+import net.minecraft.world.World;
 
 public class HealingEarring extends Item
 {
@@ -10,4 +16,11 @@ public class HealingEarring extends Item
 		setRegistryName("healing_earring");
 	}
 
+	@Override
+	public void addInformation(ItemStack stack, World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
+		tooltip.add("\u00A73" + new TextComponentTranslation("earring1").getFormattedText());
+		tooltip.add("\u00a73" + new TextComponentTranslation("earring2").getFormattedText());
+		super.addInformation(stack, worldIn, tooltip, flagIn);
+	}
+	
 }
