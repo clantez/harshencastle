f05359870bd9dc515ed36111f0722d32504b4e0f
Keny <mylankca1995@gmail.com>
2017-07-07 19:39:55 +0300

    added tooltip for pontus ring

--- src/main/java/kenijey/harshencastle/items/PontusRing.java
+++ src/main/java/kenijey/harshencastle/items/PontusRing.java
@@ -1,6 +1,12 @@
 package kenijey.harshencastle.items;
 
+import java.util.List;
+
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.TextComponentTranslation;
+import net.minecraft.world.World;
 
 public class PontusRing extends Item
 {
@@ -8,6 +14,13 @@ public class PontusRing extends Item
 	{
 		setUnlocalizedName("pontus_ring");
 		setRegistryName("pontus_ring");
+	}
+	
+	@Override
+	public void addInformation(ItemStack stack, World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
+		tooltip.add("\u00A73" + new TextComponentTranslation("pontus1").getFormattedText());
+		tooltip.add("\u00a73" + new TextComponentTranslation("pontus2").getFormattedText());
+		super.addInformation(stack, worldIn, tooltip, flagIn);
 	}
 
 }
