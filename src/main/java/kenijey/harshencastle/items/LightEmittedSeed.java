b2fe09a8e4d21e01e63a6774fc93ce075b45d03e
Keny <mylankca1995@gmail.com>
2017-07-10 12:29:00 +0300

    added tooltip info for seed

--- src/main/java/kenijey/harshencastle/items/LightEmittedSeed.java
+++ src/main/java/kenijey/harshencastle/items/LightEmittedSeed.java
@@ -1,9 +1,15 @@
 package kenijey.harshencastle.items;
 
+import java.util.List;
+
 import kenijey.harshencastle.HarshenBlocks;
 import net.minecraft.block.Block;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemSeeds;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.TextComponentTranslation;
+import net.minecraft.world.World;
 
 public class LightEmittedSeed extends ItemSeeds
 {
@@ -14,5 +20,11 @@ public class LightEmittedSeed extends ItemSeeds
 		setRegistryName("light_emitted_seed");	
 		}
 
+	@Override
+	public void addInformation(ItemStack stack, World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
+		tooltip.add("\u00A73" + new TextComponentTranslation("seed1").getFormattedText());
+		tooltip.add("\u00a73" + new TextComponentTranslation("seed2").getFormattedText());
+		super.addInformation(stack, worldIn, tooltip, flagIn);
+	}
 
 }
