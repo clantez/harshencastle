3f2445a2b1c881e3f55e186ffe55a7a479d5b05c
Keny <mylankca1995@gmail.com>
2017-07-10 11:10:49 +0300

    updated version

--- src/main/java/kenijey/harshencastle/HarshenCastle.java
+++ src/main/java/kenijey/harshencastle/HarshenCastle.java
@@ -27,7 +27,7 @@ public class HarshenCastle {
 
     public static final String MODID = "harshencastle";
     public static final String MODNAME = "Harshen Castle";
-    public static final String VERSION = "0.3.0";
+    public static final String VERSION = "0.3.1";
 
     @SidedProxy(clientSide = "kenijey.harshencastle.proxy.ClientProxy", serverSide = "kenijey.harshencastle.proxy.ServerProxy")
     public static CommonProxy proxy;
