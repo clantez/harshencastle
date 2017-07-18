b39f4b66ce4ff506624667afea1869e24c6950d4
Wyn Price <wynprice999@gmail.com>
2017-07-18 16:18:43 +0100

    made wither not work if full harshen armour

--- src/main/java/kenijey/harshencastle/handlers/HandlerSoulHarsherSword.java
+++ src/main/java/kenijey/harshencastle/handlers/HandlerSoulHarsherSword.java
@@ -1,5 +1,8 @@
 package kenijey.harshencastle.handlers;
 
+import com.google.common.collect.Lists;
+
+import kenijey.harshencastle.armor.HarshenArmor;
 import kenijey.harshencastle.items.SoulHarsherSword;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.potion.Potion;
@@ -15,9 +18,13 @@ public class HandlerSoulHarsherSword
 	{
 		try
 		{
-			if(event.getSource() instanceof EntityDamageSource && ((EntityDamageSource)event.getSource()).getTrueSource() instanceof EntityLivingBase
+			if((event.getSource() instanceof EntityDamageSource && ((EntityDamageSource)event.getSource()).getTrueSource() instanceof EntityLivingBase
 					&& (((EntityLivingBase)((EntityDamageSource)event.getSource()).getTrueSource()).getHeldItemMainhand().getItem() instanceof SoulHarsherSword)
 					|| ((EntityLivingBase)((EntityDamageSource)event.getSource()).getTrueSource()).getHeldItemOffhand().getItem() instanceof SoulHarsherSword)
+				   &&!(Lists.newArrayList(event.getEntityLiving().getArmorInventoryList().iterator()).get(3).getItem() == HarshenArmor.harshen_jaguar_armor_helmet
+					&& Lists.newArrayList(event.getEntityLiving().getArmorInventoryList().iterator()).get(2).getItem() == HarshenArmor.harshen_jaguar_armor_chestplate
+					&& Lists.newArrayList(event.getEntityLiving().getArmorInventoryList().iterator()).get(1).getItem() == HarshenArmor.harshen_jaguar_armor_leggings
+					&& Lists.newArrayList(event.getEntityLiving().getArmorInventoryList().iterator()).get(0).getItem() == HarshenArmor.harshen_jaguar_armor_boots))
 				event.getEntityLiving().addPotionEffect(new PotionEffect(Potion.getPotionById(20), 150, 1));
 		}
 		catch (ClassCastException clazz){}
