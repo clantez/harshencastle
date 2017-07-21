35a7e7ed8b04fab0b4b5bbe14c63e0053a32f4a4
Wyn Price <wynprice999@gmail.com>
2017-07-21 15:07:05 +0100

    fixed prop sword texture and added wither effect

--- src/main/java/kenijey/harshencastle/handlers/HandlerSoulHarsherSword.java
+++ src/main/java/kenijey/harshencastle/handlers/HandlerSoulHarsherSword.java
@@ -1,10 +1,14 @@
 package kenijey.harshencastle.handlers;
 
+import java.util.Arrays;
+
 import com.google.common.collect.Lists;
 
+import kenijey.harshencastle.HarshenItems;
 import kenijey.harshencastle.armor.HarshenArmors;
 import kenijey.harshencastle.items.SoulHarsherSword;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.ItemStack;
 import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.EntityDamageSource;
@@ -18,13 +22,15 @@ public class HandlerSoulHarsherSword
 	{
 		try
 		{
+			System.out.println();
+
 			if((event.getSource() instanceof EntityDamageSource && ((EntityDamageSource)event.getSource()).getTrueSource() instanceof EntityLivingBase
-					&& (((EntityLivingBase)((EntityDamageSource)event.getSource()).getTrueSource()).getHeldItemMainhand().getItem() instanceof SoulHarsherSword)
-					|| ((EntityLivingBase)((EntityDamageSource)event.getSource()).getTrueSource()).getHeldItemOffhand().getItem() instanceof SoulHarsherSword)
+					&& Arrays.asList(new ItemStack(HarshenItems.props, 1, 0).getItem(), new ItemStack(HarshenItems.soul_harsher_sword).getItem())
+					.contains(((EntityLivingBase)((EntityDamageSource)event.getSource()).getTrueSource()).getHeldItemMainhand().getItem())
 				   &&!(Lists.newArrayList(event.getEntityLiving().getArmorInventoryList().iterator()).get(3).getItem() == HarshenArmors.harshen_jaguar_armor_helmet
 					&& Lists.newArrayList(event.getEntityLiving().getArmorInventoryList().iterator()).get(2).getItem() == HarshenArmors.harshen_jaguar_armor_chestplate
 					&& Lists.newArrayList(event.getEntityLiving().getArmorInventoryList().iterator()).get(1).getItem() == HarshenArmors.harshen_jaguar_armor_leggings
-					&& Lists.newArrayList(event.getEntityLiving().getArmorInventoryList().iterator()).get(0).getItem() == HarshenArmors.harshen_jaguar_armor_boots))
+					&& Lists.newArrayList(event.getEntityLiving().getArmorInventoryList().iterator()).get(0).getItem() == HarshenArmors.harshen_jaguar_armor_boots)))
 				event.getEntityLiving().addPotionEffect(new PotionEffect(Potion.getPotionById(20), 150, 1));
 		}
 		catch (ClassCastException clazz){}
