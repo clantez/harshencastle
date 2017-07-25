ac9e71197b87161ef683089e9449c0b6acf62fe1
Wyn Price <wynprice999@gmail.com>
2017-07-25 23:32:32 +0100

    added selector classes

--- src/main/java/kenijey/harshencastle/handlers/HandlerBloodOnHurt.java
+++ src/main/java/kenijey/harshencastle/handlers/HandlerBloodOnHurt.java
@@ -1,20 +1,24 @@
 package kenijey.harshencastle.handlers;
 
+import java.util.Arrays;
 import java.util.Random;
 
 import kenijey.harshencastle.HarshenBlocks;
+import net.minecraft.entity.monster.EntityWitch;
+import net.minecraft.entity.passive.EntityVillager;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.util.EntityDamageSource;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class HandlerBloodOnHurt
 {
+	Class[] AllowedEntities = {EntityPlayerMP.class, EntityWitch.class, EntityVillager.class};
 	
 	@SubscribeEvent
 	public void onLivingHurt(LivingHurtEvent event)
 	{
-		System.out.println(event.getEntityLiving().getHeldItemMainhand().getItem().getClass());
-		if(event.getSource() instanceof EntityDamageSource && new Random().nextInt(10) == 0 )
+		if(event.getSource() instanceof EntityDamageSource && new Random().nextInt(3) == 0 && Arrays.asList(AllowedEntities).contains(event.getEntity().getClass()))
 			event.getEntity().getEntityWorld().setBlockState(event.getEntity().getPosition(), HarshenBlocks.blood_block.getDefaultState(), 3);
 	}
 }
