44ed606a567e3ea1b14bb3c3902c516ba4fdfb36
Wyn Price <wynprice999@gmail.com>
2017-07-25 22:47:15 +0100

    totem of undying now has effect if in custom slot

--- src/main/java/kenijey/harshencastle/handlers/HandlerHarshenInventoryCommon.java
+++ src/main/java/kenijey/harshencastle/handlers/HandlerHarshenInventoryCommon.java
@@ -1,12 +1,18 @@
 package kenijey.harshencastle.handlers;
 
 import java.util.Arrays;
-import java.util.Collections;
 
 import kenijey.harshencastle.base.BaseItemCustomInvEffect;
+import net.minecraft.advancements.CriteriaTriggers;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.init.Items;
 import net.minecraft.init.MobEffects;
+import net.minecraft.item.ItemStack;
 import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionEffect;
+import net.minecraft.stats.StatList;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent.PlayerTickEvent;
 import net.minecraftforge.items.ItemStackHandler;
@@ -39,6 +45,34 @@ public class HandlerHarshenInventoryCommon
 				else;
 			else
 				event.player.addPotionEffect(potionEffect);
+		}
+	}
+	
+	@SubscribeEvent
+	public void onPlayerDeath(LivingDeathEvent event)
+	{
+		if(event.getEntity() instanceof EntityPlayer)
+		{
+			EntityPlayer player = (EntityPlayer)event.getEntity();
+			if(this.handler.getStackInSlot(0).getItem() == Items.TOTEM_OF_UNDYING)
+			{
+				event.setCanceled(true);
+				if (player instanceof EntityPlayerMP)
+	            {
+	                EntityPlayerMP entityplayermp = (EntityPlayerMP)player;
+	                entityplayermp.addStat(StatList.getObjectUseStats(Items.TOTEM_OF_UNDYING));
+	                CriteriaTriggers.USED_TOTEM.trigger(entityplayermp, this.handler.getStackInSlot(0));
+	            }
+				
+				this.handler.setStackInSlot(0, ItemStack.EMPTY);
+				player.getEntityData().setTag("ItemStackHandler", handler.serializeNBT());
+				HandlerHarshenInventoryClient.instance.load(player);	
+				player.setHealth(1.0F);
+				player.clearActivePotions();
+				player.addPotionEffect(new PotionEffect(MobEffects.REGENERATION, 900, 1));
+				player.addPotionEffect(new PotionEffect(MobEffects.ABSORPTION, 100, 1));
+				player.world.setEntityState(player, (byte)35);
+			}
 		}
 	}
 }
