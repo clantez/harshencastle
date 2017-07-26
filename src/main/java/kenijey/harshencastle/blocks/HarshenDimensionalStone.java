a66f0fff328a6873b753ab899b7cc3b8481edd64
Wyn Price <wynprice999@gmail.com>
2017-07-26 14:42:19 +0100

    stopped mushrooms on stone

--- src/main/java/kenijey/harshencastle/blocks/HarshenDimensionalStone.java
+++ src/main/java/kenijey/harshencastle/blocks/HarshenDimensionalStone.java
@@ -9,10 +9,13 @@ import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.util.DamageSource;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TextComponentTranslation;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import net.minecraftforge.common.IPlantable;
 
 public class HarshenDimensionalStone extends BaseHarshenBlockCastle 
 {
@@ -21,4 +24,10 @@ public class HarshenDimensionalStone extends BaseHarshenBlockCastle
         setUnlocalizedName("harshen_dimensional_stone");
         setRegistryName("harshen_dimensional_stone");
     }
+	
+	@Override
+	public boolean canSustainPlant(IBlockState state, IBlockAccess world, BlockPos pos, EnumFacing direction,
+			IPlantable plantable) {
+		return false;
+	}
 }
