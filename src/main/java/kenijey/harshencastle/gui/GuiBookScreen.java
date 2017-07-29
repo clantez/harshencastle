6611ef2c1b01fbcf25c8c9a040d2eeb77f4e9c21
Wyn Price <wynprice999@gmail.com>
2017-07-29 19:55:28 +0100

    redid guide book as to now use images

--- src/main/java/kenijey/harshencastle/gui/GuiBookScreen.java
+++ src/main/java/kenijey/harshencastle/gui/GuiBookScreen.java
@@ -1,5 +1,6 @@
 package kenijey.harshencastle.gui;
 
+import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -7,19 +8,23 @@ import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
+
+import org.lwjgl.input.Mouse;
 
 import com.google.common.collect.Lists;
 
 import kenijey.harshencastle.HarshenCastle;
 import kenijey.harshencastle.enums.gui.EnumGuiPage;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.gui.GuiTextField;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.client.renderer.texture.TextureUtil;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.text.TextFormatting;
+import net.minecraftforge.client.model.obj.OBJModel.Texture;
 
 public class GuiBookScreen extends GuiScreen
 {
@@ -28,7 +33,8 @@ public class GuiBookScreen extends GuiScreen
 	private ArrayList<GuiButton> categoryButtons = new ArrayList<GuiButton>();
 	private ArrayList<Gui> allComponants = new ArrayList<Gui>();
 	private EnumGuiPage mode = EnumGuiPage.values()[0];
-    private List<String> lines = Lists.<String>newArrayList();
+	private int scroll;
+    
 	
 	@Override
 	public void initGui() {
@@ -38,24 +44,32 @@ public class GuiBookScreen extends GuiScreen
 	
 	@Override
 	public void drawScreen(int mouseX, int mouseY, float partialTicks) {
-		this.lines.clear();
 		this.drawDefaultBackground();
 		this.drawCenteredString(this.fontRenderer, "Harshen Castle " + mode.getName(), this.width / 2, 4, 16777215);
-		try {
-            String s = "";
-			InputStream inputstream = this.mc.getResourceManager().getResource(new ResourceLocation(HarshenCastle.MODID, "texts/category." + mode.getName().toLowerCase() + ".txt")).getInputStream();
-            BufferedReader bufferedreader = new BufferedReader(new InputStreamReader(inputstream, StandardCharsets.UTF_8));
-            String s1;
-            while ((s1 = bufferedreader.readLine()) != null)
-                this.lines.addAll(this.mc.fontRenderer.listFormattedStringToWidth(s1, 274));
-            inputstream.close();
-            for(int i = 0; i < this.lines.size(); i++)
-            	this.drawCenteredString(this.fontRenderer, this.lines.get(i), this.width / 2, 75 + (i*10), 16777215);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
 		super.drawScreen(mouseX, mouseY, partialTicks);
 	}	
+	
+	@Override
+	public void handleMouseInput() throws IOException {
+		if(Mouse.getEventDWheel() == -120)
+			scroll += 15;
+		else if(Mouse.getEventDWheel() == 120)
+			scroll -= 15;
+		super.handleMouseInput();
+	}
+	
+	@Override
+	public void drawDefaultBackground() {
+		super.drawDefaultBackground();
+		ResourceLocation location = new ResourceLocation(HarshenCastle.MODID, "textures/gui/category/" + this.mode.getName().toLowerCase()  + ".png");
+		this.mc.getTextureManager().bindTexture(location);
+        try {
+    		BufferedImage i = TextureUtil.readBufferedImage(Minecraft.getMinecraft().getResourceManager().getResource(location).getInputStream());
+			this.drawModalRectWithCustomSizedTexture(50, 50, 0, scroll, this.width - 100, this.height - 100, this.width - 100, (this.width - 100) * ((float)i.getHeight() / i.getWidth()));
+        } catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 	
 	@Override
 	protected void actionPerformed(GuiButton button){
@@ -64,6 +78,7 @@ public class GuiBookScreen extends GuiScreen
 		if(button.id > 8000)
 			this.mode = EnumGuiPage.values()[button.id - 8800];
 	}
+	
 	
 	private void categorybuttons()
 	{
