822cc5737b7eb72806ad9a0e6f25b9bdfd93715f
Wyn Price <wynprice999@gmail.com>
2017-08-21 20:22:25 +0100

    fixed dumb mistake which caused cauldron to not render liquid in GUI

--- src/main/java/kenijey/harshencastle/base/BaseJeiCategory.java
+++ src/main/java/kenijey/harshencastle/base/BaseJeiCategory.java
@@ -55,6 +55,7 @@ public abstract class BaseJeiCategory implements IRecipeCategory{
 		GlStateManager.enableAlpha();
 		GlStateManager.enableBlend();
 		overlay.draw(minecraft);
+		drawMore(minecraft);
 		GlStateManager.disableBlend();
 		GlStateManager.disableAlpha();
 	}
