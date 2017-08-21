c99bc7fb2a8741cefc64221602d8422ece573410
Wyn Price <wynprice999@gmail.com>
2017-08-21 21:14:05 +0100

    finished cauldron Gui

--- src/main/java/kenijey/harshencastle/intergration/jei/cauldron/JEICauldronCategory.java
+++ src/main/java/kenijey/harshencastle/intergration/jei/cauldron/JEICauldronCategory.java
@@ -5,6 +5,8 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 
+import kenijey.harshencastle.HarshenCastle;
+import kenijey.harshencastle.HarshenItems;
 import kenijey.harshencastle.base.BaseJeiCategory;
 import kenijey.harshencastle.enums.blocks.EnumHetericCauldronFluidType;
 import mezz.jei.api.IGuiHelper;
@@ -16,6 +18,7 @@ import mezz.jei.api.recipe.IRecipeWrapper;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.TextComponentTranslation;
 
 public class JEICauldronCategory extends BaseJeiCategory
@@ -27,6 +30,7 @@ public class JEICauldronCategory extends BaseJeiCategory
 	private static HashMap<EnumHetericCauldronFluidType, IDrawable> fluidTypes = new HashMap<>();
 	
 	private IDrawable currentFluid;
+	private IDrawable frontOfCauldron;
 	private String name;
 
 	@Override
@@ -36,26 +40,30 @@ public class JEICauldronCategory extends BaseJeiCategory
 		JEICauldronWrapper wrapper = (JEICauldronWrapper) recipeWrapper;
 		currentFluid = fluidTypes.get(wrapper.getCatalyst());
 		name = "fluid." + wrapper.getCatalyst().getName();
-		recipeLayout.getItemStacks().init(0, true, 67, 30);
+		recipeLayout.getItemStacks().init(0, true, 65, 1);
 		recipeLayout.getItemStacks().set(0, ingredients.getInputs(ItemStack.class).get(0));
-		recipeLayout.getItemStacks().init(4, false, 103, 17);
-		recipeLayout.getItemStacks().set(4, ingredients.getOutputs(ItemStack.class).get(0));
+		recipeLayout.getItemStacks().init(1, false, 66, 29);
+		recipeLayout.getItemStacks().set(1, Arrays.asList(new ItemStack(HarshenItems.ladle)));
+		recipeLayout.getItemStacks().init(2, false, 132, 41);
+		recipeLayout.getItemStacks().set(2, ingredients.getOutputs(ItemStack.class).get(0));
 	}
 	
 	@Override
 	protected void createDrawable(IGuiHelper helper) {
 		for(EnumHetericCauldronFluidType fluid : EnumHetericCauldronFluidType.values())
-			fluidTypes.put(fluid, helper.createDrawable(fluid.getResourceLoc(), 0, 0, 70, 20));
+			fluidTypes.put(fluid, helper.createDrawable(fluid.getResourceLoc(), 0, 0, 38, 14, 38, 14));
+		frontOfCauldron = helper.createDrawable(new ResourceLocation(HarshenCastle.MODID, "textures/gui/jei/cauldron-front.png"), 0, 0, 150, 110, 150, 110);
 	}
 	
 	@Override
 	protected void drawMore(Minecraft minecraft) {
-		currentFluid.draw(minecraft);
+		currentFluid.draw(minecraft, 56, 49);
+		frontOfCauldron.draw(minecraft);
 	}
 	
 	@Override
 	public List getTooltipStrings(int mouseX, int mouseY) {
-		if(mouseX < 70 && mouseY < 20)
+		if(mouseX > 56 && mouseX < 94 && mouseY > 49 && mouseY < 63)
 			return Arrays.asList(new TextComponentTranslation(name).getFormattedText());
 		return super.getTooltipStrings(mouseX, mouseY);
 	}
