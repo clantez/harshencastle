dd55fd01442af6140548c024501b669202602690
Wyn Price <wynprice999@gmail.com>
2017-08-22 12:09:35 +0100

    Fix #10

--- src/main/java/kenijey/harshencastle/blocks/HarshenDimensionalStone.java
+++ src/main/java/kenijey/harshencastle/blocks/HarshenDimensionalStone.java
@@ -1,23 +1,17 @@
 package kenijey.harshencastle.blocks;
 
-import kenijey.harshencastle.base.BaseHarshenBlockCastle;
+import kenijey.harshencastle.base.BaseHarshenBlockBreakableInPontus;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraftforge.common.IPlantable;
 
-public class HarshenDimensionalStone extends BaseHarshenBlockCastle 
+public class HarshenDimensionalStone extends BaseHarshenBlockBreakableInPontus 
 {
 	public HarshenDimensionalStone() 
 	{
         setUnlocalizedName("harshen_dimensional_stone");
         setRegistryName("harshen_dimensional_stone");
     }
-	
-	@Override
-	public boolean canSustainPlant(IBlockState state, IBlockAccess world, BlockPos pos, EnumFacing direction,
-			IPlantable plantable) {
-		return false;
-	}
 }
