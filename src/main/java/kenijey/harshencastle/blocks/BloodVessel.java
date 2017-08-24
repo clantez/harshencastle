0f17c98bf04f4e84f74e3b807adefd3a70c8abec
Wyn Price <wynprice999@gmail.com>
2017-08-24 13:09:43 +0100

    fixed models

--- src/main/java/kenijey/harshencastle/blocks/BloodVessel.java
+++ src/main/java/kenijey/harshencastle/blocks/BloodVessel.java
@@ -5,10 +5,12 @@ import java.util.List;
 
 import kenijey.harshencastle.HarshenUtils;
 import kenijey.harshencastle.tileentity.TileEntityBloodVessel;
+import kenijey.harshencastle.tileentity.TileEntityHarshenDisplayBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
@@ -20,6 +22,7 @@ import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraft.world.World;
+import net.minecraftforge.items.ItemStackHandler;
 
 public class BloodVessel extends Block implements ITileEntityProvider
 {
@@ -53,6 +56,12 @@ public class BloodVessel extends Block implements ITileEntityProvider
 	}
 	
 	@Override
+	public void onBlockPlacedBy(World worldIn, BlockPos pos, IBlockState state, EntityLivingBase placer,
+			ItemStack stack) {
+		((TileEntityBloodVessel)worldIn.getTileEntity(pos)).setBloodLevel(stack.serializeNBT().getInteger("BloodLevel"));
+	}
+	
+	@Override
 	public void breakBlock(World worldIn, BlockPos pos, IBlockState state) {
 		TileEntityBloodVessel te = (TileEntityBloodVessel) worldIn.getTileEntity(pos);
 		int amount = te.getPossibleRemove();
@@ -62,7 +71,7 @@ public class BloodVessel extends Block implements ITileEntityProvider
 		if(amount != 0)
 		{
 			NBTTagCompound nbttagcompound = new NBTTagCompound();
-	        nbttagcompound.setInteger("ItemStackHandler", amount);
+	        nbttagcompound.setInteger("BloodLevel", amount);
 	        stack.setTagCompound(nbttagcompound);
 	        stack.setStackDisplayName("§r" + getLocalizedName() + " (" + amount + "/" + max + ")");
 		}
