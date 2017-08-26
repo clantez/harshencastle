a76b4b613dc5c62b8990f1359809db2538a63b5c
Keny <mylankca1995@gmail.com>
2017-08-26 17:13:18 +0300

    completed criminal pendant

--- src/main/java/kenijey/harshencastle/items/CriminalPendant.java
+++ src/main/java/kenijey/harshencastle/items/CriminalPendant.java
@@ -2,13 +2,18 @@ package kenijey.harshencastle.items;
 
 import java.util.List;
 
+import kenijey.harshencastle.enums.inventory.EnumInventorySlots;
+import kenijey.harshencastle.interfaces.IHarshenProvider;
 import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.MobEffects;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraft.world.World;
 
-public class CriminalPendant extends Item
+public class CriminalPendant extends Item implements IHarshenProvider
 {
 	public CriminalPendant()
 	{
@@ -22,5 +27,15 @@ public class CriminalPendant extends Item
 		tooltip.add(" ");
 		tooltip.add("\u00A73" + new TextComponentTranslation("cpendant").getFormattedText());
 		super.addInformation(stack, worldIn, tooltip, flagIn);
+	}
+	
+	@Override
+	public EnumInventorySlots getSlot() {
+		return EnumInventorySlots.NECK;
+	}
+	
+	@Override
+	public void onTick(EntityPlayer player, int tick) {
+		player.addPotionEffect(new PotionEffect(MobEffects.STRENGTH, 500, 0, false, false));
 	}
 }
