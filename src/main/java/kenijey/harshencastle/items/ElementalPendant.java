a1f500d9a968a14770a921f7f01431973b9424b7
Keny <mylankca1995@gmail.com>
2017-08-27 20:40:07 +0300

    fixed pendant adds 4 hearts instead of 4hp

--- src/main/java/kenijey/harshencastle/items/ElementalPendant.java
+++ src/main/java/kenijey/harshencastle/items/ElementalPendant.java
@@ -40,7 +40,7 @@ public class ElementalPendant extends Item implements IHarshenProvider
 	@Override
 	public void onAdd(EntityPlayer player) {
 		IAttributeInstance attributeHealth = player.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH);
-		AttributeModifier modifierHealth = new AttributeModifier(UUID.fromString("d20525ee-98b2-402f-b298-61bc19a9e0c5"), "elementalPendantHealth4", 8, 0).setSaved(true);
+		AttributeModifier modifierHealth = new AttributeModifier(UUID.fromString("d20525ee-98b2-402f-b298-61bc19a9e0c5"), "elementalPendantHealth4", 4, 0).setSaved(true);
 		if(!attributeHealth.hasModifier(modifierHealth))	
 			attributeHealth.applyModifier(modifierHealth);
 		
