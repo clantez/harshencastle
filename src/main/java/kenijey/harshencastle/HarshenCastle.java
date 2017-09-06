29c71599f206f7d21c2bd69da37b3d84d8099fdf
Wyn Price <wynprice999@gmail.com>
2017-09-06 21:27:36 +0100

    updated version

--- src/main/java/kenijey/harshencastle/HarshenCastle.java
+++ src/main/java/kenijey/harshencastle/HarshenCastle.java
@@ -25,7 +25,7 @@ public class HarshenCastle {
 
     public static final String MODID = "harshencastle";
     public static final String MODNAME = "Harshen Castle";
-    public static final String VERSION = "0.9.0";
+    public static final String VERSION = "0.9.1";
 
     @SidedProxy(clientSide = "kenijey.harshencastle.proxy.ClientProxy", serverSide = "kenijey.harshencastle.proxy.ServerProxy")
     public static CommonProxy proxy;
