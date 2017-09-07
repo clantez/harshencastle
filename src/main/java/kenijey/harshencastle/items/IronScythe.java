dce63602f158deea7fcd1cfa9a7c29ec1e61dba8
Keny <mylankca1995@gmail.com>
2017-09-07 18:30:17 +0300

    finally. swing speed for scythes

--- src/main/java/kenijey/harshencastle/items/IronScythe.java
+++ src/main/java/kenijey/harshencastle/items/IronScythe.java
@@ -1,9 +1,14 @@
 package kenijey.harshencastle.items;
 
+import java.util.Collection;
+import java.util.Optional;
+import java.util.UUID;
+
 import com.google.common.collect.Multimap;
 
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.entity.ai.attributes.IAttribute;
 import net.minecraft.init.Items;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemStack;
@@ -24,17 +29,28 @@ public class IronScythe extends ItemSword
 	}
 	
 	
-	public Multimap<String, AttributeModifier> getItemAttributeModifiers(EntityEquipmentSlot equipmentSlot)
-    {
-        Multimap<String, AttributeModifier> multimap = super.getItemAttributeModifiers(equipmentSlot);
+	@Override
+	public Multimap<String, AttributeModifier> getAttributeModifiers(EntityEquipmentSlot slot, ItemStack stack) {
+		final Multimap<String, AttributeModifier> modifiers = super.getAttributeModifiers(slot, stack);
 
-        if (equipmentSlot == EntityEquipmentSlot.MAINHAND)
-        {
-            multimap.put(SharedMonsterAttributes.ATTACK_SPEED.getName(), new AttributeModifier(ATTACK_SPEED_MODIFIER, "Tool modifier", (double)this.attackSpeed, 0));
-        }
+		if (slot == EntityEquipmentSlot.MAINHAND) {
+			replaceModifier(modifiers, SharedMonsterAttributes.ATTACK_SPEED, ATTACK_SPEED_MODIFIER, 1.2);
+		}
 
-        return multimap;
-    }
+		return modifiers;
+	}
+	
+	private void replaceModifier(Multimap<String, AttributeModifier> modifierMultimap, IAttribute attribute, UUID id, double multiplier) {
+
+		final Collection<AttributeModifier> modifiers = modifierMultimap.get(attribute.getName());
+		final Optional<AttributeModifier> modifierOptional = modifiers.stream().filter(attributeModifier -> attributeModifier.getID().equals(id)).findFirst();
+
+		if (modifierOptional.isPresent()) {
+			final AttributeModifier modifier = modifierOptional.get();
+			modifiers.remove(modifier);
+			modifiers.add(new AttributeModifier(modifier.getID(), modifier.getName(), modifier.getAmount() * multiplier, modifier.getOperation()));
+		}
+	}
 	
 	@Override
 	public boolean getIsRepairable(ItemStack toRepair, ItemStack repair) {
--- src/main/java/kenijey/harshencastle/items/RaptorScythe.java
+++ src/main/java/kenijey/harshencastle/items/RaptorScythe.java
@@ -1,10 +1,15 @@
 package kenijey.harshencastle.items;
 
+import java.util.Collection;
+import java.util.Optional;
+import java.util.UUID;
+
 import com.google.common.collect.Multimap;
 
 import kenijey.harshencastle.HarshenItems;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.entity.ai.attributes.IAttribute;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemSword;
@@ -35,6 +40,29 @@ public class RaptorScythe extends ItemSword
 
         return multimap;
     }
+	
+	@Override
+	public Multimap<String, AttributeModifier> getAttributeModifiers(EntityEquipmentSlot slot, ItemStack stack) {
+		final Multimap<String, AttributeModifier> modifiers = super.getAttributeModifiers(slot, stack);
+
+		if (slot == EntityEquipmentSlot.MAINHAND) {
+			replaceModifier(modifiers, SharedMonsterAttributes.ATTACK_SPEED, ATTACK_SPEED_MODIFIER, 1.15);
+		}
+
+		return modifiers;
+	}
+	
+	private void replaceModifier(Multimap<String, AttributeModifier> modifierMultimap, IAttribute attribute, UUID id, double multiplier) {
+
+		final Collection<AttributeModifier> modifiers = modifierMultimap.get(attribute.getName());
+		final Optional<AttributeModifier> modifierOptional = modifiers.stream().filter(attributeModifier -> attributeModifier.getID().equals(id)).findFirst();
+
+		if (modifierOptional.isPresent()) {
+			final AttributeModifier modifier = modifierOptional.get();
+			modifiers.remove(modifier);
+			modifiers.add(new AttributeModifier(modifier.getID(), modifier.getName(), modifier.getAmount() * multiplier, modifier.getOperation()));
+		}
+	}
 	
 	@Override
 	public boolean getIsRepairable(ItemStack toRepair, ItemStack repair) {
