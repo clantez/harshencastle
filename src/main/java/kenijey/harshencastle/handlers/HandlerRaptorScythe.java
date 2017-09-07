fb91af538fdcc83e33cc9db5319e480c34ee0c1c
Keny <mylankca1995@gmail.com>
2017-09-07 18:58:41 +0300

    raptor scythe wither effect begun

---
+++ src/main/java/kenijey/harshencastle/handlers/HandlerRaptorScythe.java
@@ -0,0 +1,33 @@
+package kenijey.harshencastle.handlers;
+
+import com.google.common.collect.Lists;
+
+import kenijey.harshencastle.armor.HarshenArmors;
+import kenijey.harshencastle.items.HarshenProps;
+import kenijey.harshencastle.items.RaptorScythe;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.init.MobEffects;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.EntityDamageSource;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+public class HandlerRaptorScythe 
+{
+	@SubscribeEvent
+	public void attackEntity(LivingHurtEvent event)
+	{
+		try
+		{
+			if((event.getSource() instanceof EntityDamageSource && ((EntityDamageSource)event.getSource()).getTrueSource() instanceof EntityLivingBase
+					&& (((EntityLivingBase)((EntityDamageSource)event.getSource()).getTrueSource()).getHeldItemMainhand().getItem() instanceof RaptorScythe ||
+						((EntityLivingBase)((EntityDamageSource)event.getSource()).getTrueSource()).getHeldItemMainhand().getItem() instanceof HarshenProps)
+				   &&!(Lists.newArrayList(event.getEntityLiving().getArmorInventoryList().iterator()).get(3).getItem() == HarshenArmors.harshen_jaguar_armor_helmet
+					&& Lists.newArrayList(event.getEntityLiving().getArmorInventoryList().iterator()).get(2).getItem() == HarshenArmors.harshen_jaguar_armor_chestplate
+					&& Lists.newArrayList(event.getEntityLiving().getArmorInventoryList().iterator()).get(1).getItem() == HarshenArmors.harshen_jaguar_armor_leggings
+					&& Lists.newArrayList(event.getEntityLiving().getArmorInventoryList().iterator()).get(0).getItem() == HarshenArmors.harshen_jaguar_armor_boots)))
+				event.getEntityLiving().addPotionEffect(new PotionEffect(MobEffects.WITHER, 150, 1));
+		}
+		catch (ClassCastException clazz){}
+	}
+}
