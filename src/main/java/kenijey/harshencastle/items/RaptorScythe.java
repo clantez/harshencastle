63a260f4cf79f3b9844d45138a0eb6ac361cd244
Keny <mylankca1995@gmail.com>
2017-09-15 15:13:33 +0300

    improvements for swords

--- src/main/java/kenijey/harshencastle/items/RaptorScythe.java
+++ src/main/java/kenijey/harshencastle/items/RaptorScythe.java
@@ -18,7 +18,7 @@ import net.minecraftforge.common.util.EnumHelper;
 public class RaptorScythe extends ItemSword
 {
 	protected float attackSpeed;
-	private static ToolMaterial toolMaterial = EnumHelper.addToolMaterial("iron_scythe", 3, 1010, 13.5f, 11f, 30);
+	private static ToolMaterial toolMaterial = EnumHelper.addToolMaterial("iron_scythe", 3, 1010, 13.5f, 8f, 30);
 
 	public RaptorScythe() 
 	{
