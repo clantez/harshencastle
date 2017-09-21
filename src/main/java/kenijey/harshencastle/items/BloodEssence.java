221088648e00f7095df2313efc1fbcecf0adffd6
Keny <mylankca1995@gmail.com>
2017-09-21 21:42:27 +0300

    theres no error and no difference in game?

--- src/main/java/kenijey/harshencastle/items/BloodEssence.java
+++ src/main/java/kenijey/harshencastle/items/BloodEssence.java
@@ -20,7 +20,7 @@ public class BloodEssence extends Item implements IBloodSupply
 	@Override
 	public void addInformation(ItemStack stack, World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
 		tooltip.add("\u00A74" + new TextComponentTranslation("bessence1").getFormattedText());
-		super.addInformation(stack, worldIn, tooltip, flagIn);
+		//super.addInformation(stack, worldIn, tooltip, flagIn);
 	}
 	
 	@Override
