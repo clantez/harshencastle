e80907ff20232d19619905551cc0be1f29a6b47c
Wyn Price <wynprice999@gmail.com>
2017-10-11 23:31:23 +0100

    Fixed error with new inventory system on client

--- src/main/java/kenijey/harshencastle/handlers/HandlerHarshenInventory.java
+++ src/main/java/kenijey/harshencastle/handlers/HandlerHarshenInventory.java
@@ -36,8 +36,10 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 public class HandlerHarshenInventory 
 {	
 	HashMap<EntityPlayer, Integer> tickMap = new HashMap<>();
-	private static ArrayList<ItemStack> prevInv = new ArrayList<>();
-		
+	private static ArrayList<ItemStack> prevInvClient = new ArrayList<>();
+	private static ArrayList<ItemStack> prevInvServer = new ArrayList<>();
+	
+	
 	@SubscribeEvent
 	public void playerTick(PlayerTickEvent event)
 	{	
@@ -50,20 +52,22 @@ public class HandlerHarshenInventory
 			tickMap.put(event.player, 0);
 		tickMap.put(event.player, tickMap.get(event.player) + 1);
 		HarshenItemStackHandler handler = HarshenUtils.getHandler(event.player);
+		ArrayList<ItemStack> prevInv = event.side.isServer() ? prevInvServer : prevInvClient;
 		if(prevInv.size() != 0)
 			for(int slot = 0; slot < handler.getSlots(); slot++)
 				if(!prevInv.get(slot).isItemEqual(handler.getStackInSlot(slot)) && handler.getStackInSlot(slot).getItem() instanceof IHarshenProvider)
 					((IHarshenProvider)handler.getStackInSlot(slot).getItem()).onAdd(event.player);
 				else if(!prevInv.get(slot).isEmpty() && handler.getStackInSlot(slot).isEmpty() && prevInv.get(slot).getItem() instanceof IHarshenProvider)
 					((IHarshenProvider)prevInv.get(slot).getItem()).onRemove(event.player);
-		ArrayList<ItemStack> prevInv = new ArrayList<>();
+		ArrayList<ItemStack> tickHandlerInventory = new ArrayList<>();
 		for(int slot = 0; slot < handler.getSlots(); slot++)
 		{
 			if(handler.getStackInSlot(slot).getItem() instanceof IHarshenProvider)
 				((IHarshenProvider)handler.getStackInSlot(slot).getItem()).onTick(event.player, tickMap.get(event.player));
-			prevInv.add(handler.getStackInSlot(slot));
+			tickHandlerInventory.add(handler.getStackInSlot(slot));
 		}	
-		this.prevInv = prevInv;
+		prevInv.clear();
+		prevInv.addAll(tickHandlerInventory);
 	}
 	
 	private static HashMap<UUID, HarshenItemStackHandler> stackMap = new HashMap<>();
