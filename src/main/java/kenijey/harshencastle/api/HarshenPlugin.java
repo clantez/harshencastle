f10cb87246787d9c1003fa1f32586ef1f5ca4f63
Wyn Price <wynprice999@gmail.com>
2017-10-20 10:41:12 +0100

    Update HarshenPlugin.java

--- src/main/java/kenijey/harshencastle/api/HarshenPlugin.java
+++ src/main/java/kenijey/harshencastle/api/HarshenPlugin.java
@@ -3,6 +3,7 @@ package kenijey.harshencastle.api;
 /**
  * This annotation is used by HarshenCaste to detect the plugins.
  * The class must implement {@link IHarshenPlugin} and have a constructor with no arguments
+ * @author Wyn Price
  */
 public @interface HarshenPlugin {
 }
