df6fd1a88aa014cc728a7aadf43e9746fc660607
Wyn Price <wynprice999@gmail.com>
2017-10-20 21:09:25 +0100

    when ritual is build wrong, ghost blocks are now rendered where the blocks should be

--- src/main/java/kenijey/harshencastle/handlers/client/HandlerRenderError.java
+++ src/main/java/kenijey/harshencastle/handlers/client/HandlerRenderError.java
@@ -5,13 +5,13 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 
+import org.lwjgl.opengl.GL11;
+
 import kenijey.harshencastle.HarshenClientUtils;
-import kenijey.harshencastle.HarshenItems;
 import kenijey.harshencastle.objecthandlers.FaceRenderer;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GlStateManager;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.EnumHand;
+import net.minecraft.init.Blocks;
 import net.minecraftforge.client.event.RenderWorldLastEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
@@ -22,28 +22,30 @@ public class HandlerRenderError
 	@SubscribeEvent
 	public void playerOverLay(RenderWorldLastEvent event)
 	{
-		boolean flag = false;
-		for(EnumHand hand : EnumHand.values())
-			if(Minecraft.getMinecraft().player.getHeldItem(hand).isItemEqual(new ItemStack(HarshenItems.RITUAL_STICK, 1, 1)))
-				flag = true;
-		if(!flag)
-			return;
-		ArrayList<FaceRenderer> erroredPositions = new ArrayList<>(this.erroredPositions);
-		HashMap<Double, FaceRenderer> map = new HashMap<>();
-		for(FaceRenderer erroredPosition : this.erroredPositions)
-			map.put(Minecraft.getMinecraft().player.getDistanceSq(erroredPosition.getPosition()), erroredPosition);
-		ArrayList<Double> dMap = new ArrayList<>();
-		for(Double d : map.keySet()) dMap.add(d);
-		Collections.sort(dMap);
-		Collections.reverse(dMap);
-		for(Double d : dMap)
-
-			if(Minecraft.getMinecraft().world.getBlockState(map.get(d).getPosition()).getBlock() == map.get(d).getState().getBlock() &&
-			Minecraft.getMinecraft().world.getBlockState(map.get(d).getPosition()).getBlock().getMetaFromState(Minecraft.getMinecraft().world.getBlockState(map.get(d).getPosition())) == 
-					map.get(d).getState().getBlock().getMetaFromState(map.get(d).getState()))
-				this.erroredPositions.remove(map.get(d));
-		else if(map.get(d).getPosition().distanceSq(Minecraft.getMinecraft().player.posX, Minecraft.getMinecraft().player.posY, Minecraft.getMinecraft().player.posZ) < 400)
-			HarshenClientUtils.renderGhostBlock(map.get(d).getState(), map.get(d).getPosition(), new Color(1f, 1f, 1f), true, event.getPartialTicks());
-	
+		GlStateManager.pushMatrix();
+		{
+			GlStateManager.enableBlend();
+	        GlStateManager.blendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_COLOR);
+			ArrayList<FaceRenderer> erroredPositions = new ArrayList<>(this.erroredPositions);
+			HashMap<Double, FaceRenderer> map = new HashMap<>();
+			for(FaceRenderer erroredPosition : this.erroredPositions)
+				map.put(Minecraft.getMinecraft().player.getDistanceSq(erroredPosition.getPosition()), erroredPosition);
+			ArrayList<Double> dMap = new ArrayList<>();
+			for(Double d : map.keySet()) dMap.add(d);
+			Collections.sort(dMap);
+			Collections.reverse(dMap);
+			for(Double d : dMap)
+				if((Minecraft.getMinecraft().world.getBlockState(map.get(d).getPosition()).getBlock() == map.get(d).getState().getBlock() &&
+				Minecraft.getMinecraft().world.getBlockState(map.get(d).getPosition()).getBlock().getMetaFromState(Minecraft.getMinecraft().world.getBlockState(map.get(d).getPosition())) == 
+						map.get(d).getState().getBlock().getMetaFromState(map.get(d).getState())) || map.get(d).getState().getBlock() == Blocks.BARRIER && Minecraft.getMinecraft()
+						.world.getBlockState( map.get(d).getPosition()).getBlock() != Blocks.OBSIDIAN)
+					this.erroredPositions.remove(map.get(d));
+			else if(map.get(d).getPosition().distanceSq(Minecraft.getMinecraft().player.posX, Minecraft.getMinecraft().player.posY, Minecraft.getMinecraft().player.posZ) < 400)
+				HarshenClientUtils.renderGhostBlock(map.get(d).getState().getBlock() == Blocks.BARRIER ? Blocks.OBSIDIAN.getDefaultState() : map.get(d).getState(), map.get(d).getPosition(),
+						map.get(d).getState().getBlock() == Blocks.BARRIER ? Color.RED : Color.WHITE, true, event.getPartialTicks());
+	        GlStateManager.blendFunc(GL11.GL_ONE, GL11.GL_ZERO);
+			GlStateManager.disableBlend();
+		}
+		GlStateManager.popMatrix();
 	}
 }
