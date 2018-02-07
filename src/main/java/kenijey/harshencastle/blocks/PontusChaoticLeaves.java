5eca3f0ed40fe84b49ede3d55afd28c783dc7519
Keny <mylankca1995@gmail.com>
2018-02-07 17:34:16 +0300

    updated to latest recommended forge: 2611, also updated java to 8 u161, fixed incompatibility

--- src/main/java/kenijey/harshencastle/blocks/PontusChaoticLeaves.java
+++ src/main/java/kenijey/harshencastle/blocks/PontusChaoticLeaves.java
@@ -1,12 +1,30 @@
 package kenijey.harshencastle.blocks;
 
+import java.util.List;
+
 import kenijey.harshencastle.base.BaseHarshenLeaves;
+import net.minecraft.block.BlockPlanks.EnumType;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 
 public class PontusChaoticLeaves extends BaseHarshenLeaves
 {
 	@Override
 	protected String getName() {
 		return "pontus_chaotic_leaves";
+	}
+
+	@Override
+	public List<ItemStack> onSheared(ItemStack item, IBlockAccess world, BlockPos pos, int fortune) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public EnumType getWoodType(int meta) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
